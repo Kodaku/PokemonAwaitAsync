ae089b68c45d125f576aa993add3dddf296ba51a
Kodaku <66876665+Kodaku@users.noreply.github.com>
2020-09-24 10:42:14 +0200

    Update Game.ts

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -29,15 +29,15 @@ interface User {
   velocity: Velocity;
 }
 
+const url = 'http://localhost:3000';
+
 const getUserPromise = (id: number) => {
   return new Promise((resolve: (value: User) => void) => {
-    axios
-      .get(`http://localhost:3000/players/user${id}/move`)
-      .then((response) => {
-        const user = response.data as User;
-        console.log(user);
-        resolve(user);
-      });
+    axios.get(`${url}/players/user${id}/move`).then((response) => {
+      const user = response.data as User;
+      console.log(user);
+      resolve(user);
+    });
   });
 };
 
@@ -47,7 +47,7 @@ const postPosition = (player: User) => {
     // console.log(player[0].userName);
     return new Promise((resolve: (ok: string) => void) => {
       axios
-        .put(`http://localhost:3000/players/user${player[1]}/move`, {
+        .put(`${url}/players/user${player[1]}/move`, {
           userName: player[0].userName,
           userID: player[1],
           userCharacter: player[0].userCharacter,
@@ -68,7 +68,7 @@ const postPosition = (player: User) => {
 
 const getPlayersNumber = () => {
   return new Promise((resolve: (total: number) => void) => {
-    axios.get('http://localhost:3000/players/total').then((response) => {
+    axios.get(`${url}/players/total`).then((response) => {
       const data = response.data;
       resolve(data.total);
     });
